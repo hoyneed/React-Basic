f4b3b151da44bafff86ec9c391c4774260ebd034
hoyneed <hoyneed@gmail.com>
2025-07-10 15:41:58 +0900

    [Feature] React Hooks 학습- useEffect
    useEffect

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import Effect from "./Effect.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <Effect />
     </StrictMode>
 );
