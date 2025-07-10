8189a829bb81fe3ae4884c906c398eefc56aa6e8
hoyneed <hoyneed@gmail.com>
2025-07-10 14:50:43 +0900

    [Feature] React Hooks 학습- useState
    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // useState => Hooks
+    // useState는 가장 기본적인 Hook이며, 함수 컴포넌트에서도 가변적인 상태를 지닐 수 있게 해줍니다.
+    const [value, setValue] = useState<number>(5); // => 이 함수가 호출되면 배열을 반환한다.
+    const [name, setName] = useState<string>("noName");
+    const [nickname, setNickname] = useState<string>("스나이퍼팩토리");
+    // useState 함수의 파라미터에는 상태의 기본값을 넣어 줍니다. => 첫 번째 요소는 상태 값, 두 번째 요소는 상태를 설정하는 함수
+    const increment = () => setValue(value + 1);
+    const onChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    };
+    const onChangeNickname = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNickname(event.target.value);
+    };
+    return (
+        <div>
+            <div>
+                <p>
+                    현재 카운터 값은 <b>{value}</b>
+                </p>
+                <button onClick={increment}>1 증가</button>
+                <button onClick={() => setValue(value - 1)}>1 감소</button>
+            </div>
+            <div>
+                <input type="text" value={name} onChange={onChangeName} />
+                <input
+                    type="text"
+                    value={nickname}
+                    onChange={onChangeNickname}
+                />
+            </div>
+            <div>
+                <b>이름: {name}</b>
+                <b>닉네임: {nickname}</b>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
